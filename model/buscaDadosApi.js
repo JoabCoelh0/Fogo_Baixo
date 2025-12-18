96f4a30dcef55fa3216c6453b243c6ae4d99606c
José Joab <josejoab003@gmail.com>
2025-12-18 09:15:47 -0300

    Consumo API
    Consumindo API de receitas com Fetch API de forma assincrona

---
+++ model/buscaDadosApi.js
@@ -0,0 +1,11 @@
+async function fetchRecitas() {
+    try {
+        let receitas = await fetch("https://api-receitas-pi.vercel.app/receitas/todas")
+        let dadosReceitas = await receitas.json()
+        console.log(dadosReceitas);
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+fetchRecitas()
